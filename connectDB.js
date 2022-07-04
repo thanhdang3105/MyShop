841bfd186c6c3e94d20001382777a72eb6a01766
ThanhDang3105 <thanhls1235ls12@gmail.com>
2022-07-04 09:45:04 +0700

    update

--- connectDB.js
+++ connectDB.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        console.log(process.env.URI_DB)
         await mongoose.connect(process.env.URI_DB);
         console.log('Connect Successfully');
     } catch (e) {
+        console.log(e)
         console.log('Connect Fail');
     }
 }
